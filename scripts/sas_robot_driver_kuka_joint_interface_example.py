ae7c314748af9ad19a0c15c49e0b3e99e77331a8
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2024-07-16 09:48:21 +0100

    [sas_robot_driver_kuka_joint_interface_example.py] Joint position motion following a sine wave for easier visualization.

--- scripts/sas_robot_driver_kuka_joint_interface_example.py
+++ scripts/sas_robot_driver_kuka_joint_interface_example.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 # Copyright (c) 2012-2024 Murilo Marques Marinho
 #
@@ -16,14 +17,16 @@
 #    You should have received a copy of the GNU Lesser General Public License
 #    along with sas_robot_driver_kuka.  If not, see <https://www.gnu.org/licenses/>.
 #
-# ################################################################
+# #######################################################################################
 #
 #   Author: Murilo M. Marinho, email: murilomarinho@ieee.org
 #   Based on `sas_robot_driver_kuka_joint_interface_example.py` from `sas_robot_driver`
 #
-# ################################################################
+# #######################################################################################
 """
 import time
+
+from math import sin, pi
 
 import numpy
 from dqrobotics import *  # Despite what PyCharm might say, this is very much necessary or DQs will not be recognized
@@ -32,14 +35,20 @@ from dqrobotics.utils.DQ_Math import deg2rad
 from sas_common import rclcpp_init, rclcpp_Node, rclcpp_spin_some, rclcpp_shutdown
 from sas_robot_driver import RobotDriverClient
 
+from sas_core import Clock, Statistics
+
 
 def main(args=None):
     try:
         rclcpp_init()
         node = rclcpp_Node("sas_robot_driver_kuka_joint_space_example_node_cpp")
 
+        # 1 ms clock
+        clock = Clock(0.01)
+        clock.init()
+
         # Initialize the RobotDriverClient
-        rdi = RobotDriverClient(node, 'kuka_1')
+        rdi = RobotDriverClient(node, 'sas_robot_driver_kuka_composed_with_coppeliasim')
 
         # Wait for RobotDriverClient to be enabled
         while not rdi.is_enabled():
@@ -53,18 +62,28 @@ def main(args=None):
         joint_positions = rdi.get_joint_positions()
         print(f"joint positions = {joint_positions}")
 
-        # Move the joints from their current state
-        target_joint_positions = joint_positions + deg2rad([-0.5, -0.5, -0.5, -0.5, -0.5, -0.5, -0.5])
-        rdi.send_target_joint_positions(target_joint_positions)
+        # For some iterations. Note that this can be stopped with CTRL+C.
+        for i in range(0, 5000):
+            clock.update_and_sleep()
 
-        # Wait until the joints move close enough
-        while not numpy.allclose(target_joint_positions, rdi.get_joint_positions(), rtol=10e-1):
+            # Move the joints
+            target_joint_positions = joint_positions + deg2rad([10.0*sin(i/(50.0*pi))]*7)
+            print(target_joint_positions)
+            rdi.send_target_joint_positions(target_joint_positions)
+
             rclcpp_spin_some(node)
-            time.sleep(0.1)
 
-        # Read the values sent by the RobotDriverServer
-        joint_positions = rdi.get_joint_positions()
-        print(f"joint positions = {joint_positions}")
+        # Statistics
+        print("Statistics for the entire loop")
+        print("  Mean computation time: {}".format(clock.get_statistics(
+            Statistics.Mean, Clock.TimeType.Computational)
+        ))
+        print("  Mean idle time: {}".format(clock.get_statistics(
+            Statistics.Mean, Clock.TimeType.Idle)
+        ))
+        print("  Mean effective thread sampling time: {}".format(clock.get_statistics(
+            Statistics.Mean, Clock.TimeType.EffectiveSampling)
+        ))
 
         rclcpp_shutdown()
 
